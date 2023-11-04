3b5a2ca1acf038a46fb43e1248f724e7c9aa11b4
Nguyễn Tiến Đạt <88433859+TienDatGl@users.noreply.github.com>
2023-11-04 13:19:58 +0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -181,12 +181,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AWS_ACCESS_KEY_ID = 'AKIAUCMYFMTRLS5SLV7U'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'traveladvisor'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'ap-southeast-2'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
